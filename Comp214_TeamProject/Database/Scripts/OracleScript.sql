299f9881a68df47a5094d7c2de632be123337477
Rodrigo Januário da Silva <rjdsilv@gmail.com>
2018-01-10 16:32:50 -0500

    Finished the Database Script, Fixed some application bugs.

--- Comp214_TeamProject/Database/Scripts/OracleScript.sql
+++ Comp214_TeamProject/Database/Scripts/OracleScript.sql
@@ -112,7 +112,7 @@ CREATE TABLE TBUB_BOOK_RENTAL (
 		NOT NULL
 ,	RENTAL_RETURN_DATE
         DATE
-        NOT NULL
+        NULL
 ,	USER_ID
         DECIMAL(11)
         NOT NULL
@@ -230,14 +230,40 @@ CREATE INDEX IX_BOOKS_AUTHORS_01 ON TBUB_BOOKS_AUTHORS(AUTHOR_ID);
  * Inserting Data.
  */
 INSERT INTO	TBUB_USERS (USER_EMAIL, USER_PASSWORD, USER_FIRST_NAME, USER_LAST_NAME, USER_ROLE, USER_CREATE_DATE)
-VALUES( 'admin_01@admin.com', 'Admin1234', 'Administrator', '01', 'ADMIN', CURRENT_TIMESTAMP);
+VALUES( 'admin_01@admin.com', 'Admin1234', 'Administrator 1', '01', 'ADMIN', CURRENT_TIMESTAMP);
+INSERT INTO	TBUB_USERS (USER_EMAIL, USER_PASSWORD, USER_FIRST_NAME, USER_LAST_NAME, USER_ROLE, USER_CREATE_DATE)
+VALUES( 'admin_02@admin.com', 'Admin1234', 'Administrator 2', '02', 'ADMIN', CURRENT_TIMESTAMP);
+INSERT INTO	TBUB_USERS (USER_EMAIL, USER_PASSWORD, USER_FIRST_NAME, USER_LAST_NAME, USER_ROLE, USER_CREATE_DATE)
+VALUES( 'admin_03@admin.com', 'Admin1234', 'Administrator 3', '03', 'ADMIN', CURRENT_TIMESTAMP);
+INSERT INTO	TBUB_USERS (USER_EMAIL, USER_PASSWORD, USER_FIRST_NAME, USER_LAST_NAME, USER_ROLE, USER_CREATE_DATE)
+VALUES( 'user_01@admin.com', 'User1234', 'User 1', '01', 'USER', CURRENT_TIMESTAMP);
+INSERT INTO	TBUB_USERS (USER_EMAIL, USER_PASSWORD, USER_FIRST_NAME, USER_LAST_NAME, USER_ROLE, USER_CREATE_DATE)
+VALUES( 'user_02@admin.com', 'User1234', 'User 2', '01', 'USER', CURRENT_TIMESTAMP);
 
 INSERT INTO TBUB_CATEGORIES(CATEGORY_NAME) VALUES('Fantasy');
 INSERT INTO TBUB_CATEGORIES(CATEGORY_NAME) VALUES('Magic');
+INSERT INTO TBUB_CATEGORIES(CATEGORY_NAME) VALUES('Information Technology');
+INSERT INTO TBUB_CATEGORIES(CATEGORY_NAME) VALUES('Artificial Intelligence');
+INSERT INTO TBUB_CATEGORIES(CATEGORY_NAME) VALUES('Game Programming');
+INSERT INTO TBUB_CATEGORIES(CATEGORY_NAME) VALUES('Mathematics');
+INSERT INTO TBUB_CATEGORIES(CATEGORY_NAME) VALUES('History');
+INSERT INTO TBUB_CATEGORIES(CATEGORY_NAME) VALUES('Thriller');
 
 INSERT INTO TBUB_AUTHORS(AUTHOR_NAME) VALUES('J. K. Rowling');
+INSERT INTO TBUB_AUTHORS(AUTHOR_NAME) VALUES('Peter Norvig Stuart J Russell');
+INSERT INTO TBUB_AUTHORS(AUTHOR_NAME) VALUES('Jason Gregory');
+INSERT INTO TBUB_AUTHORS(AUTHOR_NAME) VALUES('Robert Nystrom');
+INSERT INTO TBUB_AUTHORS(AUTHOR_NAME) VALUES('Eric Lengyel');
+INSERT INTO TBUB_AUTHORS(AUTHOR_NAME) VALUES('George R.R. Martin');
+INSERT INTO TBUB_AUTHORS(AUTHOR_NAME) VALUES('Bernard Cornwell ');
 
 INSERT INTO TBUB_PUBLISHERS(PUBLISHER_NAME) VALUES('Bloomsbury Childre''s Books');
+INSERT INTO TBUB_PUBLISHERS(PUBLISHER_NAME) VALUES('Pearson India');
+INSERT INTO TBUB_PUBLISHERS(PUBLISHER_NAME) VALUES('A K Peters/CRC Press');
+INSERT INTO TBUB_PUBLISHERS(PUBLISHER_NAME) VALUES('Genever Benning');
+INSERT INTO TBUB_PUBLISHERS(PUBLISHER_NAME) VALUES('Course Technology PTR');
+INSERT INTO TBUB_PUBLISHERS(PUBLISHER_NAME) VALUES('Bantam; Media Tie In edition');
+INSERT INTO TBUB_PUBLISHERS(PUBLISHER_NAME) VALUES('HarperCollins');
 
 INSERT INTO TBUB_BOOKS(BOOK_ISBN, BOOK_TITLE, BOOK_DESCRIPTION, BOOK_PUBLICATION_DATE, BOOK_EDITION, BOOK_IS_AVAILABLE, BOOK_QUANTITY_AVAILABLE, BOOK_PAGES, PUBLISHER_ID, BOOK_IMG_URL_01, BOOK_IMG_URL_02, BOOK_IMG_URL_03, BOOK_IMG_URL_04, BOOK_IMG_URL_05)
 VALUES(
@@ -283,10 +309,106 @@ VALUES(
 	NULL,
 	NULL);
 
+INSERT INTO TBUB_BOOKS(BOOK_ISBN, BOOK_TITLE, BOOK_DESCRIPTION, BOOK_PUBLICATION_DATE, BOOK_EDITION, BOOK_IS_AVAILABLE, BOOK_QUANTITY_AVAILABLE, BOOK_PAGES, PUBLISHER_ID, BOOK_IMG_URL_01, BOOK_IMG_URL_02, BOOK_IMG_URL_03, BOOK_IMG_URL_04, BOOK_IMG_URL_05)
+VALUES(
+	9789332543515, 'Artificial Intelligence : A Modern Approach',
+	'Brand New. Artificial Intlelligence theory book.',
+	'2015-09-12', 3, 1, 5, 795, 2,
+	'https://images-na.ssl-images-amazon.com/images/I/71MgQIMnAWL.jpg',
+	NULL,
+	NULL,
+	NULL,
+	NULL);
+
+INSERT INTO TBUB_BOOKS(BOOK_ISBN, BOOK_TITLE, BOOK_DESCRIPTION, BOOK_PUBLICATION_DATE, BOOK_EDITION, BOOK_IS_AVAILABLE, BOOK_QUANTITY_AVAILABLE, BOOK_PAGES, PUBLISHER_ID, BOOK_IMG_URL_01, BOOK_IMG_URL_02, BOOK_IMG_URL_03, BOOK_IMG_URL_04, BOOK_IMG_URL_05)
+VALUES(
+	9781466560017, 'Game Engine Architecture',
+	'Hailed as a "must-have textbook" (CHOICE, January 2010), the first edition of Game Engine Architecture provided readers with a complete guide to the theory and practice of game engine software development. Updating the content to match today’s landscape of game engine architecture, this second edition continues to thoroughly cover the major components that make up a typical commercial game engine.',
+	'2014-08-15', 2, 1, 2, 1052, 3,
+	'https://images-na.ssl-images-amazon.com/images/I/51P2wXledgL.jpg',
+	'https://images-na.ssl-images-amazon.com/images/I/51wlTrj0q1L.jpg',
+	'https://images-na.ssl-images-amazon.com/images/I/41GV0KOj4ZL.jpg',
+	NULL,
+	NULL);
+
+INSERT INTO TBUB_BOOKS(BOOK_ISBN, BOOK_TITLE, BOOK_DESCRIPTION, BOOK_PUBLICATION_DATE, BOOK_EDITION, BOOK_IS_AVAILABLE, BOOK_QUANTITY_AVAILABLE, BOOK_PAGES, PUBLISHER_ID, BOOK_IMG_URL_01, BOOK_IMG_URL_02, BOOK_IMG_URL_03, BOOK_IMG_URL_04, BOOK_IMG_URL_05)
+VALUES(
+	9780990582908, 'Game Programming Patterns',
+	'The biggest challenge facing many game programmers is completing their game. Most game projects fizzle out, overwhelmed by the complexity of their own code. Game Programming Patterns tackles that exact problem. Based on years of experience in shipped AAA titles, this book collects proven patterns to untangle and optimize your game, organized as independent recipes so you can pick just the patterns you need.',
+	'2014-11-02', 1, 1, 1, 354, 4,
+	'https://images-na.ssl-images-amazon.com/images/I/71Kfg2zTisL.jpg',
+	'https://images-na.ssl-images-amazon.com/images/I/81NmSfv4NoL.jpg',
+	'https://images-na.ssl-images-amazon.com/images/I/31mSaO0vaFL.jpg',
+	NULL,
+	NULL);
+
+INSERT INTO TBUB_BOOKS(BOOK_ISBN, BOOK_TITLE, BOOK_DESCRIPTION, BOOK_PUBLICATION_DATE, BOOK_EDITION, BOOK_IS_AVAILABLE, BOOK_QUANTITY_AVAILABLE, BOOK_PAGES, PUBLISHER_ID, BOOK_IMG_URL_01, BOOK_IMG_URL_02, BOOK_IMG_URL_03, BOOK_IMG_URL_04, BOOK_IMG_URL_05)
+VALUES(
+	9781435458864, 'Mathematics for 3D Game Programming and Computer Graphics',
+	'This updated third edition illustrates the mathematical concepts that a game developer needs to develop 3D computer graphics and game engines at the professional level. It starts at a fairly basic level in areas such as vector geometry and linear algebra, and then progresses to more advanced topics in 3D programming such as illumination and visibility determination. Particular attention is given to derivations of key results, ensuring that the reader is not forced to endure gaps in the theory. The book assumes a working knowledge of trigonometry and calculus, but also includes sections that review the important tools used from these disciplines, such as trigonometric identities, differential equations, and Taylor series.',
+	'2011-07-02', 3, 1, 3, 624, 5,
+	'https://images-na.ssl-images-amazon.com/images/I/61k9lBQjfWL.jpg',
+	NULL,
+	NULL,
+	NULL,
+	NULL);
+
+INSERT INTO TBUB_BOOKS(BOOK_ISBN, BOOK_TITLE, BOOK_DESCRIPTION, BOOK_PUBLICATION_DATE, BOOK_EDITION, BOOK_IS_AVAILABLE, BOOK_QUANTITY_AVAILABLE, BOOK_PAGES, PUBLISHER_ID, BOOK_IMG_URL_01, BOOK_IMG_URL_02, BOOK_IMG_URL_03, BOOK_IMG_URL_04, BOOK_IMG_URL_05)
+VALUES(
+	9780345535528, 'Game of Thrones 5-Book Boxed Set (Song of Ice and Fire Series)',
+	'For the first time, all five novels in the epic fantasy series that inspired HBO’s Game of Thrones are together in one boxed set. An immersive entertainment experience unlike any other, A Song of Ice and Fire has earned George R. R. Martin—dubbed “the American Tolkien” by Time magazine—international acclaim and millions of loyal readers. Now here is the entire monumental cycle:<br /><br />A GAME OF THRONES<br/>A CLASH OF KINGS<br />A STORM OF SWORDS<br />A FEAST FOR CROWS<br />A DANCE WITH DRAGONS',
+	'2013-10-29', 1, 1, 2, 5216, 6,
+	'https://images-na.ssl-images-amazon.com/images/I/61Y-vYqyqYL.jpg',
+	'https://images-na.ssl-images-amazon.com/images/I/919-FLL37TL.jpg',
+	'https://images-na.ssl-images-amazon.com/images/I/81sBQOYFeSL.jpg',
+	'https://images-na.ssl-images-amazon.com/images/I/81tE51KeB7L.jpg',
+	'https://images-na.ssl-images-amazon.com/images/I/81tE51KeB7L.jpg');
+
+INSERT INTO TBUB_BOOKS(BOOK_ISBN, BOOK_TITLE, BOOK_DESCRIPTION, BOOK_PUBLICATION_DATE, BOOK_EDITION, BOOK_IS_AVAILABLE, BOOK_QUANTITY_AVAILABLE, BOOK_PAGES, PUBLISHER_ID, BOOK_IMG_URL_01, BOOK_IMG_URL_02, BOOK_IMG_URL_03, BOOK_IMG_URL_04, BOOK_IMG_URL_05)
+VALUES(
+	9780007218011, 'The Last Kingdom Series, Book 1 - The Last Kingdom',
+	'The first book in Bernard Cornwell’s epic and bestselling series on the making of England and the fate of his great hero, Uhtred of Bebbanburg. BBC2’s major Autumn 2015 TV show THE LAST KINGDOM is based on the first two books in the series.',
+	'2006-03-20', 1, 1, 5, 352, 7,
+	'https://images-na.ssl-images-amazon.com/images/I/515enqa9E6L.jpg',
+	'https://images-na.ssl-images-amazon.com/images/I/81v3TpKxfrL.jpg',
+	NULL,
+	NULL,
+	NULL);
+
+INSERT INTO TBUB_BOOKS(BOOK_ISBN, BOOK_TITLE, BOOK_DESCRIPTION, BOOK_PUBLICATION_DATE, BOOK_EDITION, BOOK_IS_AVAILABLE, BOOK_QUANTITY_AVAILABLE, BOOK_PAGES, PUBLISHER_ID, BOOK_IMG_URL_01, BOOK_IMG_URL_02, BOOK_IMG_URL_03, BOOK_IMG_URL_04, BOOK_IMG_URL_05)
+VALUES(
+	9780007149933, 'The Last Kingdom Series, Book 2 - The Pale Horseman',
+	'The second book in Bernard Cornwell’s epic and bestselling series on the making of England and the fate of his great hero, Uhtred of Bebbanburg. BBC2’s major Autumn 2015 TV show THE LAST KINGDOM is based on the first two books in the series.',
+	'2007-07-08', 1, 1, 3, 464, 7,
+	'https://images-na.ssl-images-amazon.com/images/I/51nK9GYj4hL.jpg',
+	'https://images-na.ssl-images-amazon.com/images/I/81BhVT8b1HL.jpg',
+	NULL,
+	NULL,
+	NULL);
+
+INSERT INTO TBUB_BOOKS(BOOK_ISBN, BOOK_TITLE, BOOK_DESCRIPTION, BOOK_PUBLICATION_DATE, BOOK_EDITION, BOOK_IS_AVAILABLE, BOOK_QUANTITY_AVAILABLE, BOOK_PAGES, PUBLISHER_ID, BOOK_IMG_URL_01, BOOK_IMG_URL_02, BOOK_IMG_URL_03, BOOK_IMG_URL_04, BOOK_IMG_URL_05)
+VALUES(
+	9780007219704, 'The Last Kingdom Series, Book 3 - The Lords of the ',
+	'The third book in Bernard Cornwell’s epic and bestselling series on the making of England and the fate of his great hero, Uhtred of Bebbanburg. BBC2’s major Autumn 2015 TV show THE LAST KINGDOM is based on the first two books in the series.',
+	'2006-03-27', 2, 1, 7, 400, 7,
+	'https://images-na.ssl-images-amazon.com/images/I/51IaKfJoMpL.jpg',
+	'https://images-na.ssl-images-amazon.com/images/I/81s5D-QQ4yL.jpg',
+	'https://images-na.ssl-images-amazon.com/images/I/41TgdMs80ZL.jpg',
+	NULL,
+	NULL);
+
 INSERT INTO TBUB_BOOKS_AUTHORS(BOOK_ISBN, AUTHOR_ID) VALUES(9781408845646, 1);
 INSERT INTO TBUB_BOOKS_AUTHORS(BOOK_ISBN, AUTHOR_ID) VALUES(9781408845653, 1);
 INSERT INTO TBUB_BOOKS_AUTHORS(BOOK_ISBN, AUTHOR_ID) VALUES(9781408845660, 1);
 INSERT INTO TBUB_BOOKS_AUTHORS(BOOK_ISBN, AUTHOR_ID) VALUES(9781408890769, 1);
+INSERT INTO TBUB_BOOKS_AUTHORS(BOOK_ISBN, AUTHOR_ID) VALUES(9789332543515, 2);
+INSERT INTO TBUB_BOOKS_AUTHORS(BOOK_ISBN, AUTHOR_ID) VALUES(9781466560017, 3);
+INSERT INTO TBUB_BOOKS_AUTHORS(BOOK_ISBN, AUTHOR_ID) VALUES(9780990582908, 4);
+INSERT INTO TBUB_BOOKS_AUTHORS(BOOK_ISBN, AUTHOR_ID) VALUES(9781435458864, 5);
+INSERT INTO TBUB_BOOKS_AUTHORS(BOOK_ISBN, AUTHOR_ID) VALUES(9780345535528, 6);
+INSERT INTO TBUB_BOOKS_AUTHORS(BOOK_ISBN, AUTHOR_ID) VALUES(9780007218011, 7);
+INSERT INTO TBUB_BOOKS_AUTHORS(BOOK_ISBN, AUTHOR_ID) VALUES(9780007149933, 7);
+INSERT INTO TBUB_BOOKS_AUTHORS(BOOK_ISBN, AUTHOR_ID) VALUES(9780007219704, 7);
 
 INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9781408845646, 1);
 INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9781408845646, 2);
@@ -296,6 +418,26 @@ INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9781408845660, 
 INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9781408845660, 2);
 INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9781408890769, 1);
 INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9781408890769, 2);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9789332543515, 3);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9789332543515, 4);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9781466560017, 3);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9781466560017, 5);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9780990582908, 3);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9780990582908, 5);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9781435458864, 3);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9781435458864, 5);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9781435458864, 6);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9780345535528, 1);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9780345535528, 2);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9780007218011, 1);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9780007218011, 7);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9780007218011, 8);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9780007149933, 1);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9780007149933, 7);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9780007149933, 8);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9780007219704, 1);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9780007219704, 7);
+INSERT INTO TBUB_BOOKS_CATEGORIES(BOOK_ISBN, CATEGORY_ID) VALUES(9780007219704, 8);
 
 COMMIT;
 
@@ -365,6 +507,7 @@ CREATE OR REPLACE PROCEDURE SPUB_LOGIN (
     UserEmail        IN  VARCHAR2
 ,   UserPassword     IN  CHAR
 ,   UserId			 OUT DECIMAL
+,	UserRole		 OUT VARCHAR2
 ,   UserFirstName    OUT VARCHAR2
 ,   UserLastName     OUT VARCHAR2
 )
@@ -372,9 +515,11 @@ IS
 BEGIN
 
     SELECT  USER_ID
+	,		USER_ROLE
     ,       USER_FIRST_NAME
     ,       USER_LAST_NAME
     INTO    UserId
+	,		UserRole
     ,       UserFirstName
     ,       UserLastName
     FROM    TBUB_USERS
@@ -394,6 +539,7 @@ CREATE OR REPLACE PROCEDURE SPUB_REGISTER (
 ,   UserFirstName    IN  VARCHAR2
 ,   UserLastName     IN  VARCHAR2
 ,   UserId           OUT DECIMAL
+,	UserRole		 OUT VARCHAR2
 )
 IS
     lvUserRegistered   DECIMAL(1, 0);
@@ -405,11 +551,13 @@ BEGIN
 		INTO	TBUB_USERS (
 			USER_EMAIL
 		,	USER_PASSWORD
+		,	USER_ROLE
 		,	USER_FIRST_NAME
 		,	USER_LAST_NAME
 		) VALUES (
 			UserEmail
 		,	UserPassword
+		,	'USER'
 		,	UserFirstName
 		,	UserLastName
 		);
@@ -417,7 +565,9 @@ BEGIN
         COMMIT;
 
         SELECT  USER_ID
+		,		USER_ROLE
         INTO    UserId
+		,		UserRole
         FROM    TBUB_USERS
         WHERE   USER_EMAIL  =   UserEmail;
     ELSE
